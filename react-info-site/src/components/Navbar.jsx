311e331e06d790fb01dc9a5c5320ef83de426f40
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2022-12-18 22:32:04 -0600

    Styled the nav bar of the project

--- react-info-site/src/components/Navbar.jsx
+++ react-info-site/src/components/Navbar.jsx
@@ -6,7 +6,15 @@ Hint: use the Figma file for the most accurate peek at the design
 */
 
 import React from "react";
+import Icon from "../assets/react-icon-small.png";
+import "./navbar.css";
 
 export default function Navbar() {
-  return <h1>Navbar component</h1>;
+  return (
+    <nav>
+      <img src={Icon} />
+      <h3>ReactFacts</h3>
+      <h4>React Course - Project 1</h4>
+    </nav>
+  );
 }
